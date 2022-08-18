01d7304c0fed1ef0a381d5ac8f2508e925ad7e5d
Dipanshu Rewatkar <71503619+DipanshuRewatkar@users.noreply.github.com>
2022-08-18 18:31:13 +0530

    Add files via upload

---
+++ c.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main{
+
+    int a, b;
+    printf("enter the number :");
+    scanf("%d",a);
+
+    printf("enter the number :");
+    scanf("%d",b);
+
+    printf("the sum is %d:"a+b);
+
+
+
+
+    return 0 ;
+}
